4df1e3891c7fd7ea8d002290e081884c77abea9c
David Gilkeson <davidgilkeson8@gmail.com>
2020-04-17 23:08:57 +0100

    Added charts and graphs

--- src/App.js
+++ src/App.js
@@ -7,19 +7,27 @@ import { fetchData } from "./api"; // Just folder name as it automatically searc
 class App extends React.Component {
   state = {
     data: {},
+    country: "",
   };
 
   async componentDidMount() {
     const fetchedData = await fetchData();
-    this.setState({ data: fetchedData });
+    this.setState({
+      data: fetchedData,
+    });
   }
+
+  handleCountryChange = async (country) => {
+    // Fetch the data
+    // Set the state
+  };
 
   render() {
     const { data } = this.state;
     return (
       <div className={styles.container}>
-        <Cards data={data} />
-        <CountryPicker />
+        <Cards data={data} />{" "}
+        <CountryPicker handleCountryChange={this.handleCountryChange} />{" "}
         <Chart />
       </div>
     );
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
 
-const Charts = () => {
-  return <h1> Charts </h1>;
+import styles from "./Chart.module.css";
+const Chart = () => {
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    fetchAPI();
+  });
+
+  const lineCart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255, 0, 0, 0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  return <div className={styles.container}>{lineCart}</div>;
 };
-export default Charts;
+export default Chart;
