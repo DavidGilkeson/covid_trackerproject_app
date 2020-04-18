0661d2575f74000d1b587ec57269c01603d03df3
David Gilkeson <davidgilkeson8@gmail.com>
2020-04-18 14:11:49 +0100

    Updates

---
+++ precache-manifest.1b0a3a7937b4b7396dd18bf9805824fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5468a36ec035d64c5ee90b826dd41b8e",
+    "url": "/covid_trackerproject_app/index.html"
+  },
+  {
+    "revision": "3d217741293fa1951b48",
+    "url": "/covid_trackerproject_app/static/css/main.55061e0f.chunk.css"
+  },
+  {
+    "revision": "c4c7bfe418e5dd37a1e1",
+    "url": "/covid_trackerproject_app/static/js/2.d0cb8f5d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_trackerproject_app/static/js/2.d0cb8f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d217741293fa1951b48",
+    "url": "/covid_trackerproject_app/static/js/main.240f48e3.chunk.js"
+  },
+  {
+    "revision": "0a446e5241a1e5386eca",
+    "url": "/covid_trackerproject_app/static/js/runtime-main.19a2943b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_trackerproject_app/static/media/image.d7265326.png"
+  }
+]);
